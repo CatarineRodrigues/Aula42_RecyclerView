f08cbf8fb30108fc6c2577d665855d06a00b8eb3
CatarineRodrigues <catarine_pacheco@hotmail.com>
2022-06-07 14:41:51 -0300

    Implementar intent

--- Marvel/app/src/main/java/br/com/zup/marvel/MainActivity.kt
+++ Marvel/app/src/main/java/br/com/zup/marvel/MainActivity.kt
@@ -1,6 +1,7 @@
 package br.com.zup.marvel
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.AttributeSet
@@ -24,6 +25,10 @@ class MainActivity : AppCompatActivity() {
         exibirRecyclerView()
         adicionarHeroiListaHeroi()
 
+        binding.rvListaHerois.setOnClickListener{
+            val intent = Intent(this, Detalhe::class.java)
+            startActivity(intent)
+        }
     }
 
     private fun adicionarHeroiListaHeroi() {
